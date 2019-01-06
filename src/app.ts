cc6e99c950a8eeb5d16a64a479bb419c7a6d1132
masak <oicwam.orz1228@gmail.com>
2019-01-06 13:57:21 +0900

    app.tsのミドルウェアに関して説明コメントを付与

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+// http://info-i.net/express-body-parser#body-parser
 import * as bodyParser from 'body-parser';
 import { mainRoutes } from "./routes/MainRoutes";
 
@@ -13,10 +14,34 @@ class App {
 
     private config() {
 
+        /**
+         * <title>
+         *   bodyParser.json([options])
+         * <description>
+         *   データをJSONとして取得するミドルウェアを作成する。
+         * <url>
+         *   https://github.com/expressjs/body-parser#bodyparserjsonoptions
+         */
         this.app.use(bodyParser.json());
 
+        /**
+         * <title>
+         *   bodyParser.urlencoded([options])
+         * <description>
+         *   メディアタイプがapplication/x-www-form-urlencodedのデータをオブジェクトとして取得するミドルウェアを作成する。
+         *     ※application/x-www-form-urlencodedについて→https://developer.mozilla.org/ja/docs/Web/HTTP/Methods/POST
+         * <options>
+         *   extended:
+         *     URLエンコードされたデータをクエリ文字列ライブラリとqsライブラリのどちらで解析するかを選択する。
+         *       ※違いについて→http://info-i.net/querystring-parse
+         *     true : qsライブラリを使用(default)
+         *     false: クエリ文字列ライブラリを使用
+         * <url>
+         *   https://github.com/expressjs/body-parser#bodyparserurlencodedoptions
+         */
         this.app.use(bodyParser.urlencoded( {extended: false }));
 
+        // パスに/mainがあるときこのルーターが呼ばれる
         this.app.use('/main', mainRoutes);
     }
 }
